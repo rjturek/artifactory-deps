f54863a4e54c9d51311ad41f3bbc70a9deae8bdb
Richard Turek <rjt1357@gmail.com>
2016-09-04 22:21:13 -0400

    type assertion on interface map

--- artifactory-deps.go
+++ artifactory-deps.go
@@ -6,15 +6,22 @@ import
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"reflect"
 )
 
 //    "io"
-
 //    "os"
 
 func main() {
-	artifacts := getArtifacts("ggg", "aaa", "vvv")
-	dependencies := getDependencies(artifacts)
+	artifacts := getArtifactsByGAV("ggg", "aaa", "vvv")
+	fmt.Println(reflect.TypeOf(artifacts))
+
+	// dependencies := getDependencies(artifacts)
+	// fmt.Println(artifacts)
+	a := artifacts.(map[string]interface{}) // type assertion
+	for k := range a {
+		fmt.Println(k)
+	}
 }
 
 func check(e error) {
@@ -23,17 +30,17 @@ func check(e error) {
 	}
 }
 
-func getArtifactsByGAV(groupID string, artifactID string, version string) artifacts {
+func getArtifactsByGAV(groupID string, artifactID string, version string) interface{} {
 	fmt.Println(groupID)
 	fmt.Println(artifactID)
 	fmt.Println(version)
 
 	dat, err := ioutil.ReadFile("./sampleOutput/output.json")
 	check(err)
-	fmt.Print(string(dat))
+	// fmt.Print(string(dat))
 
 	var artifacts interface{}
-	err = json.Unmarshal(b, &artifacts)
+	err = json.Unmarshal(dat, &artifacts)
 	check(err)
 	return artifacts
 }

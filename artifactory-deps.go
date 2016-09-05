cf53160e66473a7c042bde74618f69e4bda8a3f6
Richard Turek <rjt1357@gmail.com>
2016-09-05 09:57:44 -0400

    deeper into the map

--- artifactory-deps.go
+++ artifactory-deps.go
@@ -12,16 +12,43 @@ import
 //    "io"
 //    "os"
 
+var baseURL string
+var group string
+var artifact string
+var version string
+
+var consumerMap = make(map[string][]string, 10)
+
 func main() {
-	artifacts := getArtifactsByGAV("ggg", "aaa", "vvv")
+	processCommandLine()
+	produceReport()
+}
+
+func processCommandLine() {
+	// fake for now
+	baseURL = "http://pirepo/artifactory/api/"
+	group = "ggg"
+	artifact = "aaa"
+	version = "vvv"
+}
+
+func produceReport() {
+	artifacts := getArtifactsByGAV(group, artifact, version)
 	fmt.Println(reflect.TypeOf(artifacts))
 
 	// dependencies := getDependencies(artifacts)
 	// fmt.Println(artifacts)
-	a := artifacts.(map[string]interface{}) // type assertion
-	for k := range a {
-		fmt.Println(k)
+	jsonMap := artifacts.(map[string]interface{}) // type assertion
+	for key, val := range jsonMap {
+		fmt.Println(key)
+		fmt.Println(reflect.TypeOf(val))
+		artifactMap := val.(map[string]interface{})
+		for k, v := range artifactMap {
+			fmt.Println(k)
+			fmt.Println(reflect.TypeOf(v))
+		}
 	}
+
 }
 
 func check(e error) {
